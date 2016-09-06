571e676dc34629f36296e60a5922e251ac940eef
Spencer Chen <syc10@pitt.edu>
2016-09-06 16:16:53 -0400

    This is the test from the setup instructions. I hope this was submitted properly. I see a new folder that I did not remember creating manually titled "vcs.xml"

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,16 @@
+# your code and comments
+#
+# Template for code submission
+# name       : Spencer Chen
+# email      : syc10@pitt.edu
+# date       : 09/06/2016
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+#
+# Starting out with Python, Chapter 1, Exercise 3
+#
+# Notes:
+#
+# I hope this was submitted properly
