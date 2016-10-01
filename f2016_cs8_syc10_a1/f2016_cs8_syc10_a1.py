e306e500ee6c1879c1b355bbc1aa7142bda79651
Spencer Chen <syc10@pitt.edu>
2016-09-30 23:05:21 -0400

    Assignment 1, final version of program

--- f2016_cs8_syc10_a1/f2016_cs8_syc10_a1.py
+++ f2016_cs8_syc10_a1/f2016_cs8_syc10_a1.py
@@ -11,7 +11,7 @@
 # In the comments, I tried to describe what I intended the for following line to do.
 
 #Ask the user for preferred system of input, choices being "USC" or "Metric" as strings
-Unit = input("USC or Metric?")
+Unit = input("USC or Metric? ")
 #If the user inputs "USC" as the measurement system, it will run the following code
 if Unit == "USC":
 #Ask user for distance in miles, assign to variable Distance as a float
@@ -40,20 +40,40 @@ Consumption = Distance / Gas
 #Answer will be assigned to cm and will be a float
 cm = (Gas_Metric * 100)/ Distance_Metric
 
-
+#compare cm value to given scale
+#check if cm is larger than 20
 if cm > 20:
+#if so, assign "Extremely Poor" to variable Rating
     Rating = "Extremely Poor"
+#check if cm is larger than 15. Since anything larger than 20 was already ruled out, this checks for 15>cm>=20
 elif cm >15:
+#if so, assign "Poor" to variable Rating
     Rating = "Poor"
+#check if cm is larger than 10. Since anything larger than 15 was already ruled out, this checks for 10>cm>=15
 elif cm > 10:
+#if so, assign "Average" to variable Rating
     Rating = "Average"
+#check if cm is larger than 8. Since anything larger than 10 was already ruled out, this checks for 8>cm>=10
 elif cm > 8:
+#if so, assign "Good" to variable Rating
     Rating = "Good"
+#Anything else should be less or equal to 8, so assign "Excellent" to variable Rating
 else:
     Rating = "Excellent"
-
-print(Distance, Distance_Metric, Gas, Gas_Metric, Consumption, cm, Rating)
-
-#print("Distance" + format(Distance,'.3f') + format(Distance_Metric,'.3f'))
+#Here it goes...printing the results
+#I lined up the columns using tabs and spaces
+print("\t\t\t\t\t\t\t   USC\t\t\t   Metric\n"
+#I printed both USC distance and metric distance with an accuracy of 3
+#The width was 10 (6 digits + 1 decimal point + 3 decimal places). I then labelled with units
+      "Distance _____________ :" + format(Distance, '10.3f') + " miles   " + \
+      format (Distance_Metric, '10.3f') + " Km\n"\
+#I did a similar thing for USC gas and metric gas
+      "Gas __________________ :" + format(Gas, '10.3f') + " gallons " + \
+      format (Gas_Metric, '10.3f') + " Liters\n"\
+#I did a similar thing for USC consumption and metric consumption
+      "Consumption __________ :" + format(Consumption, '10.3f') + " mpg     " + \
+      format (cm, '10.3f') + " 1/100Km\n\n"\
+#I finally printed the gas consumption rating
+      "Gas Consumption Rating : " + Rating)
 
 
