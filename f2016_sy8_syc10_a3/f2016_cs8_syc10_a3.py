13c2c044707efce2b440099c08ac47c104a788e1
Spencer Chen <syc10@pitt.edu>
2016-11-22 23:42:07 -0500

    Assignment 3

--- f2016_sy8_syc10_a3/f2016_cs8_syc10_a3.py
+++ f2016_sy8_syc10_a3/f2016_cs8_syc10_a3.py
@@ -31,6 +31,8 @@
 
 #This function opens the Master input file, then opens the files that are in the Master. It then processes a lot of the data
 def openFile(MIF):
+#Creates dictionary for everything
+    dictionary = {}
 #Initializes number of input files, total number of lines read, total distance run to 0
     numfiles = 0
     lines = 0
@@ -40,28 +42,43 @@ def openFile(MIF):
 #Finds name of file, strips '\n'
         datafile = line.rstrip('\n')
 #Uses processFile function to return ????????????????
-        numlines, pd = processFile(datafile)
+        numlines, pd, pdict = processFile(datafile)
 #Adds 1 to number of input files per file
         numfiles += 1
 #Adds total number of lines obtained from data file
         lines += numlines
 #Adds partial distances to total distance
         td += pd
+#Compiles master dictionary with every name
+        dictionary.update(pdict)
 #Prints results
     print('Number of Input files read   : ' + str(numfiles))
     print('Total number of lines read   : ' + str(lines))
     print('\nTotal distance run           : ' + str(td))
+    print('\nmax distance run             : ' + str(dictionary.get(max(dictionary, key=dictionary.get))))
+    print('  by participant             : ' + str(max(dictionary, key=dictionary.get)))
+    print('\nmin distance run             : ' + str(dictionary.get(min(dictionary, key=dictionary.get))))
+    print('  by participant             : ' + str(min (dictionary, key=dictionary.get)))
+    print('\nTotal number of participants : ' + str(len(dictionary)))
+    print('Number of participants\n with multiple records       : ')
 
 #This function processes the individual data files
 def processFile(datafile):
+#Creates an empty dictionary for each file
     dict = {}
+#Opens the data file listed in master
     file = open(datafile, 'r')
+#Initializes number of lines and partial distance to 0
     numlines = 0
     pd = 0
+#Goes through each line with for loop
     for line in file:
+#Adds 1 to number of lines for each line processed
         numlines += 1
+#Strips '\n' off of each line and splits name from distance
         name = line.rstrip('\n')
         temp = line.split(',')
+#
         readname = temp[0]
         readnum = temp[1]
         pd += float(readnum)
@@ -75,7 +92,7 @@ def processFile(datafile):
 
 
 
-    return numlines, pd
+    return numlines, pd, dict
 
 
 #Main function
